29b9d82578984ce6801fd40149bd44c639f4a0ec
scary101 <scary101@yandex.ru>
2023-10-30 10:47:19 +0300

    ...

---
+++ kaya/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using System.Xml.Linq;
+
+namespace kaya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int finaly = 0;
+            List< string> maintort = new List<string>() {"  1. Форма коржей", "  2.Цвет коржей", "  3.Вкус начинки", "  4.Декор", "  5.Цвет гллазури", "  Завершить заказ"};
+            List<Tort> doptort = new List<Tort>();
+            Tort forma = new Tort();
+            forma.name.Add("  1.Круглая");
+            forma.name.Add("  2.Квадратная");
+            forma.name.Add("  3.Треугольная");
+            forma.coast.Add(200);
+            forma.coast.Add(150);
+            forma.coast.Add(250);
+            doptort.Add(forma);
+            
+            Tort colour = new Tort();
+            colour.name.Add("  1.Белый");
+            colour.name.Add("  2.Красный");
+            colour.name.Add("  3.Серо-буро-малиновый");
+            colour.coast.Add(100);
+            colour.coast.Add(150);
+            colour.coast.Add(1000);
+            doptort.Add(colour);
+
+            Tort vkus = new Tort();
+            vkus.name.Add("  1.Банан-клубника");
+            vkus.name.Add("  2.Карамельный БУМ");
+            vkus.name.Add("  3.Радуга вкусов");
+            vkus.coast.Add(450);
+            vkus.coast.Add(300);
+            vkus.coast.Add(600);
+            doptort.Add(vkus);
+
+            Tort decor = new Tort();
+            decor.name.Add("  1.DOTA 2");
+            decor.name.Add("  2.Brawl Stars");
+            decor.name.Add("  3.Однотон");
+            decor.coast.Add(10000);
+            decor.coast.Add(15000);
+            decor.coast.Add(250);
+            doptort.Add(decor);
+
+            Tort colourCream = new Tort();
+            colourCream.name.Add("  1.Белый");
+            colourCream.name.Add("  2.Синий");
+            colourCream.name.Add("  3.Красный");
+            colourCream.coast.Add(250);
+            colourCream.coast.Add(320);
+            colourCream.coast.Add(99);
+            doptort.Add(colourCream);
+
+
+
+
+
+            ConsoleKeyInfo key;
+            KeyPos belka = new KeyPos();
+            Zakaz ready = new Zakaz();
+            List<string> gotovo = new List<string>();
+            string cena;
+            bool end = true;
+            while (end != false)
+            {
+                Menu.MainMenu(maintort,finaly, gotovo);
+                key = Console.ReadKey();
+                belka = Strelka.arrow(key, maintort, 0, end);
+                end = belka.end;
+
+                key = belka.key;
+                int poz = belka.poz - 1;
+                ready = Zakaz.zakaz(key, doptort, poz, finaly, maintort);
+                finaly = ready.finaly;
+                cena = Convert.ToString(ready.cena);
+                ready.gotovo = ready.gotovo[4..];
+                ready.gotovo = ready.gotovo + " - " + cena + ",  ";
+                gotovo.Add(ready.gotovo);
+
+            }
+            Zakaz.Save(gotovo, finaly);
+
+        }
+    }
+}
---
+++ kaya/tort.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace kaya
+{
+    internal class Tort
+    {
+        public List<string> name = new List<string>();
+        public List<int> coast = new List<int>();  
+    }
+    
+    static class Strelka
+    {
+        private static int  poz = 1;
+        public static KeyPos arrow(ConsoleKeyInfo key, List<string> main,  int min, bool end)
+        {
+            int max = main.Count() + 1;
+
+            while (key.Key != ConsoleKey.Enter)
+            {
+                Console.SetCursorPosition(0, poz);
+                Console.WriteLine("  ");
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    poz--;
+                    if (poz <= min)
+                    {
+                        poz = max - 1;
+                    }
+                }
+                else if (key.Key == ConsoleKey.DownArrow)
+                {
+                    poz++;
+                    if (poz >= max)
+                    {
+                        poz = min + 1;
+                    }
+
+                }
+
+                Console.SetCursorPosition(0, poz);
+                Console.WriteLine("->");
+                key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    Console.Clear();
+                    end = false;
+                }
+
+            }
+            return new KeyPos { poz = poz, key = key, end = end };
+        }
+    }
+
+    class Zakaz
+    {
+        public int finaly;
+        public string gotovo;
+        public int cena;
+        
+        public static Zakaz zakaz(ConsoleKeyInfo key, List<Tort> dop, int poz1, int finalyCoast, List<string> main)
+        {
+            bool a = true;
+            Console.Clear();
+            Menu.DopMenu(dop, poz1, main);
+            KeyPos belka = new KeyPos();
+            int poz = 0;
+            while (belka.key.Key != ConsoleKey.Enter )
+            {
+                key = Console.ReadKey(); 
+                belka = Strelka.arrow(key, dop[poz1].name, 0, a);
+                key = belka.key;
+                poz = belka.poz;
+            }
+
+            belka = Strelka.arrow(key, dop[poz1].name, 0, a);
+            if (key.Key == ConsoleKey.Enter) { finalyCoast = finalyCoast + dop[poz1].coast[poz - 1]; }
+
+            return new Zakaz { finaly = finalyCoast, gotovo = dop[poz1].name[poz - 1], cena = dop[poz1].coast[poz - 1] };
+
+        }
+        public static void Save(List<string> a, int b)
+        {
+            DateTime now = DateTime.Now;
+            string time = Convert.ToString(now);
+            string txt ="\n"+"\nЗаказ от: " + time +  "\nВаш заказ состоит из: " + string.Join(" ", a.ToArray()) + "\nИтоговая цена: " + Convert.ToString(b);
+            File.AppendAllText("C:\\Users\\user\\Desktop\\Заказы\\Заказ.txt", txt);
+        }
+
+    }
+    class Menu
+    {
+        public static void MainMenu(List<string> a, int finalyCoast, List<string> name)
+        {
+            Console.Clear();
+            Console.WriteLine("Добро пожаловать в мою ПЕКАРНЮ!!! Заходи, не стесняйся");
+            foreach (string s in a)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("  " + finalyCoast);
+            string concat = string.Join(" ", name.ToArray());
+            Console.WriteLine("Ваш заказ:" + concat);
+        }
+        public static void DopMenu(List<Tort> a, int poz, List <string> main)
+        {
+            Console.WriteLine(main[poz] + ":");
+            for (int i = 0; i < a[poz].coast.Count; i++)
+            {
+                Console.WriteLine(a[poz].name[i] + "  -  " + a[poz].coast[i]);
+            }
+        }
+    }
+    class KeyPos
+    {
+        public int poz;
+        public ConsoleKeyInfo key;
+        public bool end;
+    }
+}
